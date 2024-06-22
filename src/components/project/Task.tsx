c0c019bea846ea6a3a63f6379a26fcf01fd580c8
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-22 19:13:42 +0800

    Refactor Task component to use draggable functionality

--- src/components/project/Task.tsx
+++ src/components/project/Task.tsx
@@ -1,30 +1,24 @@
-import { motion } from "framer-motion";
-import TaskDropIndicator from "./TaskDropIndicator";
-import { BoardsTable, TasksTable } from "../../supabase/supabase-types";
-
-const Task = ({
-  task,
-  board,
-  onDragStart,
-}: {
-  task: TasksTable;
-  board: BoardsTable;
-  onDragStart: (e: any, task: TasksTable) => void;
-}) => {
+import { TasksTable } from "../../supabase/supabase-types";
+import { Draggable } from "@hello-pangea/dnd";
+const Task = ({ task, idx }: { task: TasksTable; idx: number }) => {
   return (
-    <>
-      <TaskDropIndicator taskOrder={task.order} board={board} beforeId="1" />
-      <motion.article
-        layout
-        layoutId={task.id}
-        draggable="true"
-        onDragStart={(e) => onDragStart(e, task)}
-        className="bg-[#2f2f2f] rounded-sm text-sm p-2 font-semibold hover:bg-[#3f3f3f] hover:cursor-grab active:cursor-grabbing"
-        key={task.id}
-      >
-        {task.name}
-      </motion.article>
-    </>
+    <Draggable draggableId={task.id} index={idx}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          className="bg-[#2f2f2f] rounded-sm text-sm mt-1 p-2 font-semibold hover:bg-[#3f3f3f] hover:cursor-grab active:cursor-grabbing"
+        >
+          <div
+            {...provided.dragHandleProps}
+            className="flex items-center gap-2"
+          >
+            {task.name}
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
