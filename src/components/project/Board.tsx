dc82c6aaaa408bff1f7faed4e9f5870422fc450f
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-23 10:21:56 +0800

    Add onDeleteTask and onAddTask props to Board component

--- src/components/project/Board.tsx
+++ src/components/project/Board.tsx
@@ -1,21 +1,24 @@
-import { BoardWithTasks } from "../../supabase/supabase-types";
+import { BoardWithTasks, TasksTable } from "../../supabase/supabase-types";
 import { Draggable, Droppable } from "@hello-pangea/dnd";
 import Task from "./Task";
+import CreateTask from "./CreateTask";
 type Props = {
   idx: number;
   board: BoardWithTasks;
+  onDeleteTask: (taskId: string) => Promise<void>;
+  onAddTask: (task: TasksTable) => Promise<void>;
 };
 
-const Board = ({ board, idx }: Props) => {
+const Board = ({ board, idx, onDeleteTask, onAddTask }: Props) => {
   return (
     <Draggable draggableId={board.id} index={idx}>
       {(provided) => (
         <div
           {...provided.draggableProps}
           ref={provided.innerRef}
-          className="h-full flex py-4 px-2 gap-1"
+          className="h-full flex py-2  px-2 gap-1 active:border-opacity-100 border-[1px] border-[#2f2f2f] rounded-md border-opacity-0"
         >
-          <div {...provided.dragHandleProps} className="h-full">
+          <div {...provided.dragHandleProps} className="">
             <h4 className="text-md mb-1 rounded-sm w-fit px-1 font-semibold">
               {board.name}
             </h4>
@@ -24,18 +27,28 @@ const Board = ({ board, idx }: Props) => {
                 <div
                   {...provided.droppableProps}
                   ref={provided.innerRef}
-                  className="h-full bg-[#202020] w-[270px] rounded-sm p-2 pt-[2px]"
+                  className=" bg-[#202020] w-[270px] rounded-sm p-2 pt-[2px]"
                 >
                   {board.tasks.map((task, idx) => {
-                    return <Task idx={idx} task={task} key={task.id} />;
+                    return (
+                      <Task
+                        onDeleteTask={onDeleteTask}
+                        idx={idx}
+                        task={task}
+                        key={task.id}
+                      />
+                    );
                   })}
                   {provided.placeholder}
+                  <CreateTask
+                    tasks={board.tasks}
+                    board_id={board.id}
+                    onAddTask={onAddTask}
+                  />
                 </div>
               )}
             </Droppable>
-            {/* <CreateTask board={board} /> */}
           </div>
-          <div className="w-[1.5px] bg-emerald-500 opacity-0 h-full"></div>
         </div>
       )}
     </Draggable>
