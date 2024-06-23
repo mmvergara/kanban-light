c6df2f35d361ef33c88433af4494a5a3c7c8173f
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-23 10:22:03 +0800

    Refactor Task component to include onDeleteTask prop

--- src/components/project/Task.tsx
+++ src/components/project/Task.tsx
@@ -1,6 +1,11 @@
 import { TasksTable } from "../../supabase/supabase-types";
 import { Draggable } from "@hello-pangea/dnd";
-const Task = ({ task, idx }: { task: TasksTable; idx: number }) => {
+type Props = {
+  task: TasksTable;
+  idx: number;
+  onDeleteTask: (taskId: string) => Promise<void>;
+};
+const Task = ({ task, idx, onDeleteTask }: Props) => {
   return (
     <Draggable draggableId={task.id} index={idx}>
       {(provided) => (
@@ -8,13 +13,21 @@ const Task = ({ task, idx }: { task: TasksTable; idx: number }) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
-          className="bg-[#2f2f2f] rounded-sm text-sm mt-1 p-2 font-semibold hover:bg-[#3f3f3f] hover:cursor-grab active:cursor-grabbing"
+          className="bg-[#2f2f2f] group w-full rounded-sm text-sm mt-1 p-2 font-semibold hover:bg-[#3f3f3f] hover:cursor-grab active:cursor-grabbing"
         >
           <div
             {...provided.dragHandleProps}
             className="flex items-center gap-2"
           >
             {task.name}
+          </div>
+          <div className="flex justify-end">
+            <button
+              onClick={() => onDeleteTask(task.id)}
+              className="text-xs group-hover:opacity-100 p-1 opacity-0 text-gray-500 ml-auto  hover:text-red-700"
+            >
+              delete
+            </button>
           </div>
         </div>
       )}
