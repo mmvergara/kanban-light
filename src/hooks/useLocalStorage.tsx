6e41f24b6ad77e9430d1b650af4cf197fe2e7218
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-23 10:22:15 +0800

    Add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, Dispatch<SetStateAction<T>>] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue: Dispatch<SetStateAction<T>> = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === key) {
+        setStoredValue(JSON.parse(event.newValue || "null"));
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, [key]);
+
+  return [storedValue, setValue];
+}
