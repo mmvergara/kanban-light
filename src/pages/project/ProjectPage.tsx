f480a8e80559817000c88a23353bd55208ca66ef
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-23 10:22:07 +0800

    Refactor code to improve performance and readability

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -1,10 +1,17 @@
 import { Navigate, useParams } from "react-router-dom";
 import supabase from "../../supabase";
-import { KanbanBoard } from "../../supabase/supabase-types";
+import {
+  BoardsTable,
+  BoardWithTasks,
+  KanbanBoard,
+  TasksTable,
+} from "../../supabase/supabase-types";
 import { useEffect, useState } from "react";
 import Board from "../../components/project/Board";
 import { DragDropContext, Droppable, DropResult } from "@hello-pangea/dnd";
 import { toast } from "react-toastify";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useSession } from "../../context/SessionContext";
 
 const reorder = <T,>(list: T[], startIdx: number, endIdx: number) => {
   const result = Array.from(list);
@@ -15,9 +22,70 @@ const reorder = <T,>(list: T[], startIdx: number, endIdx: number) => {
 const ProjectPage = () => {
   const params = useParams();
   const projectId = params.projectId!;
-  const [projectData, setProjectData] = useState<KanbanBoard | null>(null);
-  const [boards, setBoards] = useState<KanbanBoard["boards"] | null>(null);
+  const { user } = useSession();
+  const [localBoards, setLocalBoards] = useLocalStorage<KanbanBoard["boards"]>(
+    `${projectId}-boards`,
+    []
+  );
+  const [localProjectData, setLocalProjectData] =
+    useLocalStorage<KanbanBoard | null>(`${projectId}-projectData`, null);
+  const [projectData, setProjectData] = useState<KanbanBoard | null>(
+    localProjectData
+  );
+  const [boards, setBoards] = useState<KanbanBoard["boards"]>(localBoards);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [newBoardName, setNewBoardName] = useState<string>("");
+
+  const handleAddNewBoard = async () => {
+    if (!user) return;
+    if (newBoardName.trim() === "") return;
+    const newBoard = {
+      name: newBoardName,
+      order: boards.length,
+      owner_id: user?.id,
+      project_id: projectId,
+    };
+    const { error, data } = await supabase
+      .from("boards")
+      .insert(newBoard)
+      .select("*")
+      .maybeSingle();
+    if (error) {
+      console.error(error);
+      toast.error("Failed to create board");
+      setBoards(boards);
+    }
+    if (data) {
+      const newBoard: BoardWithTasks = { ...data, tasks: [] };
+      setBoards([...boards, newBoard]);
+    }
+  };
+
+  const handleAddTask = async (task: TasksTable) => {
+    const newBoards = boards.map((board) => {
+      if (board.id === task.board_id) {
+        if (!board.tasks) {
+          board.tasks = [];
+        }
+        board.tasks.push(task);
+      }
+      return board;
+    });
+
+    setBoards(newBoards);
+
+    const { error } = await supabase
+      .from("tasks")
+      .insert(task)
+      .select("*")
+      .maybeSingle();
+    if (error) {
+      console.error(error);
+      toast.error("Failed to create task");
+      setBoards(boards);
+    }
+  };
+
   const handleGetProjectData = async () => {
     const { data, error } = await supabase
       .from("projects")
@@ -35,15 +103,65 @@ const ProjectPage = () => {
       setIsLoading(false);
     }
   };
+
+  const handleUpdateBoards = async (board: BoardsTable[]) => {
+    setIsLoading(true);
+    const { error } = await supabase.from("boards").upsert(board);
+    if (error) {
+      console.error(error);
+      toast.error("Failed to update boards");
+      setBoards(boards);
+    }
+    setIsLoading(false);
+  };
+
+  const handleUpdateTasks = async (tasks: TasksTable[]) => {
+    setIsLoading(true);
+    const { error } = await supabase.from("tasks").upsert(tasks);
+    if (error) {
+      console.error(error);
+      toast.error("Failed to update tasks");
+      setBoards(boards);
+    }
+    setIsLoading(false);
+  };
+
+  const handleDeleteTask = async (taskId: string) => {
+    setBoards((prevBoards) => {
+      const newBoards = prevBoards.map((board) => {
+        if (!board.tasks) return board;
+        const newTasks = board.tasks.filter((task) => task.id !== taskId);
+        return { ...board, tasks: newTasks };
+      });
+      return newBoards;
+    });
+
+    setIsLoading(true);
+    const { error } = await supabase.from("tasks").delete().eq("id", taskId);
+    setIsLoading(false);
+    if (error) {
+      console.error(error);
+      toast.error("Failed to delete task");
+      return;
+    }
+  };
+
+  useEffect(() => {
+    setLocalBoards(boards);
+    setLocalProjectData(projectData);
+  }, [boards, projectData]);
+
   useEffect(() => {
     handleGetProjectData();
   }, [projectId]);
 
-  if (isLoading) return <div>Loading...</div>;
-  if (!boards) {
-    toast.error("Project not found");
-    return <Navigate to="/" />;
-  }
+  // if (isLoading) return <div>Loading...</div>;
+  // if (!boards) {
+  //   toast.error("Project not found");
+  //   return <Navigate to="/" />;
+  // }
+
+  if (!isLoading && !projectData) return <Navigate to="/" />;
 
   const handleDragEnd = (result: DropResult) => {
     const { destination, source, type } = result;
@@ -65,29 +183,19 @@ const ProjectPage = () => {
       );
 
       setBoards(newBoards);
-      //TODO: Trigger server update
-
-      // NewBoards with tasks removed
-      const newBoardsData = newBoards.map((board) => {
+      // Trigger Boards update
+      const newBoardsData: BoardsTable[] = newBoards.map((board) => {
         return {
           id: board.id,
           name: board.name,
           order: board.order,
           owner_id: board.owner_id,
           project_id: board.project_id,
+          created_at: board.created_at,
         };
       });
 
-      supabase
-        .from("boards")
-        .upsert(newBoardsData)
-        .then((res) => {
-          if (res.error) {
-            console.error(res.error);
-            toast.error("Failed to update boards");
-            setBoards(boards);
-          }
-        });
+      handleUpdateBoards(newBoardsData);
     }
 
     // User moves a task
@@ -129,22 +237,13 @@ const ProjectPage = () => {
         reorderedCards.forEach((task, idx) => {
           task.order = idx;
         });
-        console.log(reorderedCards);
 
         sourceBoard.tasks = reorderedCards;
         setBoards(newBoardsData);
-        // TODO Trigger server update
 
-        supabase
-          .from("tasks")
-          .upsert(sourceBoard.tasks)
-          .then((res) => {
-            if (res.error) {
-              console.error(res.error);
-              toast.error("Failed to update tasks");
-              setBoards(boards);
-            }
-          });
+        // Trigger Tasks update
+
+        handleUpdateTasks(sourceBoard.tasks);
       } else {
         // Moving task to another board
         const [removed] = sourceBoard.tasks.splice(source.index, 1);
@@ -164,33 +263,28 @@ const ProjectPage = () => {
 
         setBoards(newBoardsData);
 
-        supabase
-          .from("tasks")
-          .upsert(sourceBoard.tasks)
-          .then((res) => {
-            if (res.error) {
-              console.error(res.error);
-              toast.error("Failed to update tasks");
-              setBoards(boards);
-            }
-          });
+        // Trigger Tasks update for source board
+        handleUpdateTasks(sourceBoard.tasks);
 
-        supabase
-          .from("tasks")
-          .upsert(destinationBoard.tasks)
-          .then((res) => {
-            if (res.error) {
-              console.error(res.error);
-              toast.error("Failed to update tasks");
-              setBoards(boards);
-            }
-          });
+        // Trigger Tasks update for destination board
+        handleUpdateTasks(destinationBoard.tasks);
       }
     }
   };
+
   return (
     <div className="m-4 flex flex-col overflow-hidden overflow-x-auto">
-      <h1 className="pl-1 text-xl">{projectData?.name}</h1>
+      <div className="flex flex-col pl-1 ">
+        <h1 className="text-xl">{projectData?.name}</h1>
+        <span
+          className="text-xs text-gray-600 font-medium"
+          style={{
+            opacity: isLoading ? 1 : 0,
+          }}
+        >
+          syncing...
+        </span>
+      </div>
       <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId="boards" type="board" direction="horizontal">
           {(provided) => (
@@ -199,10 +293,38 @@ const ProjectPage = () => {
               ref={provided.innerRef}
               className="h-full flex   gap-1"
             >
-              {boards.map((board, idx) => {
-                return <Board board={board} key={board.id} idx={idx} />;
-              })}
+              {boards
+                .sort((a, b) => a.order - b.order)
+                .map((board, idx) => {
+                  return (
+                    <Board
+                      onAddTask={handleAddTask}
+                      onDeleteTask={handleDeleteTask}
+                      board={board}
+                      key={board.id}
+                      idx={idx}
+                    />
+                  );
+                })}
               {provided.placeholder}
+              <form className="w-[254px] h-fit p-2 flex items-center gap-1">
+                <input
+                  type="text"
+                  value={newBoardName}
+                  onChange={(e) => setNewBoardName(e.target.value)}
+                  className="w-full focus:bg-[#282828] bg-[#191919] rounded-sm h-full  border-none outline-none text-sm text-zinc-400 p-1 "
+                  placeholder="+ New Board"
+                />
+                {newBoardName.length > 0 && (
+                  <button
+                    type="button"
+                    onClick={handleAddNewBoard}
+                    className="p-[5px] px-[8px] text-xs rounded-sm  bg-[#282828] hover:text-emerald-500"
+                  >
+                    add
+                  </button>
+                )}
+              </form>
             </div>
           )}
         </Droppable>
