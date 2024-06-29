fac4134fe6e7737b36d38ff61f9d38474f675be4
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-29 09:09:30 +0800

    chore: Add sound effect and delete functionality to Task component

--- src/components/project/Task.tsx
+++ src/components/project/Task.tsx
@@ -1,11 +1,26 @@
 import { TasksTable } from "../../supabase/supabase-types";
 import { Draggable } from "@hello-pangea/dnd";
+import Pop1Sound from "../../assets/sounds/pop1.wav";
+import Pop2Sound from "../../assets/sounds/pop2.wav";
+import Pop3Sound from "../../assets/sounds/pop3.wav";
 type Props = {
   task: TasksTable;
   idx: number;
   onDeleteTask: (taskId: string) => Promise<void>;
 };
 const Task = ({ task, idx, onDeleteTask }: Props) => {
+  const sounds = [Pop1Sound, Pop2Sound, Pop3Sound];
+  const getRandomSound = () => {
+    return sounds[Math.floor(Math.random() * sounds.length)];
+  };
+  const handleDelete = async () => {
+    const audio = new Audio(getRandomSound());
+    audio.volume = 0.2;
+    audio.playbackRate = 1;
+    audio.play();
+    await onDeleteTask(task.id);
+  };
+
   return (
     <Draggable draggableId={task.id} index={idx}>
       {(provided) => (
@@ -23,10 +38,25 @@ const Task = ({ task, idx, onDeleteTask }: Props) => {
           </div>
           <div className="flex justify-end">
             <button
-              onClick={() => onDeleteTask(task.id)}
-              className="text-xs group-hover:opacity-100 p-1 opacity-0 text-gray-500 ml-auto  hover:text-red-500 hover:font-semibold"
+              onClick={handleDelete}
+              className="text-xs group-hover:opacity-100 p-1 opacity-0 ml-auto  text-red-500 hover:font-semibold transition-colors"
             >
-              delete
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="hover-fill-red"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <path d="m15 9-6 6" />
+                <path d="m9 9 6 6" />
+              </svg>
             </button>
           </div>
         </div>
