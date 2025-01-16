9fb156937e8b3304db202f2382dac998f7f141dd
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-16 13:13:55 +0800

    refactor: Replace getBindingByDiscordUserId with checkBinding for improved error handling and user feedback

--- bot/src/repo/users.ts
+++ bot/src/repo/users.ts
@@ -1,24 +1,50 @@
+import type { CacheType, ChatInputCommandInteraction } from "discord.js";
+import { errorEmbedReply } from "../messages";
 import { supabase } from "../supabase";
 
 export type UserID = string & { readonly brand: unique symbol };
 
-export const getBindingByDiscordUserId = async (discordUserId: string) => {
+export const checkBinding = async (
+  interaction: ChatInputCommandInteraction<CacheType>
+) => {
+  const discordUserId = interaction.user.id;
   const { data, error } = await supabase
     .from("binding")
-    .select("owner_id, discord_user_id, active_project, active_column")
+    .select("owner_id, discord_user_id, active_project")
     .eq("discord_user_id", Number.parseInt(discordUserId))
     .single();
 
   if (error || !data) {
     if (error.code === "PGRST116") {
-      return {
-        error: "You are not binded to an account, use /bind command to bind",
-      } as const;
+      await interaction.reply(
+        errorEmbedReply(
+          "You are not binded to an account, use /bind command to bind"
+        )
+      );
+    } else {
+      await interaction.reply(
+        errorEmbedReply("An error occurred while fetching your projects")
+      );
     }
-    return { error: "An error occurred while fetching your projects" } as const;
+    return null;
   }
 
-  return { binding: data } as const;
+  if (!data.active_project) {
+    await interaction.reply(
+      errorEmbedReply(
+        "No active project found, use /activate-project to activate a project"
+      )
+    );
+    return null;
+  }
+
+  const verifiedBinding = data as {
+    owner_id: UserID;
+    active_project: string;
+    discord_user_id: number;
+  };
+
+  return verifiedBinding;
 };
 
 export const bindDiscordUser = async (
