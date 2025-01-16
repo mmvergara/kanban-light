e60b4e4029573ed5939aaddbec3e85a6d4dec413
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-16 08:59:03 +0800

    feat: Implement delete-project command to remove active projects and improve error handling

--- bot/src/commands/projects/delete-project.ts
+++ bot/src/commands/projects/delete-project.ts
@@ -1,28 +1,32 @@
 import { type CacheType, type ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { errorEmbedReply, infoEmbedReply } from "../../messages";
-import { createProject } from "../../repo/projects";
-import { getUserIdByDiscordUserId } from "../../repo/users";
+import { deleteProjectById } from "../../repo/projects";
+import { getBindingByDiscordUserId, type UserID } from "../../repo/users";
 
 export const data = new SlashCommandBuilder()
   .setName("delete-project")
-  .setDescription("Delete a project")
-  .addStringOption(option => option.setName("name").setDescription("The name of the project").setRequired(true));
+  .setDescription("Delete active project");
 
 export const execute = async (interaction: ChatInputCommandInteraction<CacheType>) => {
   const discordUserId = interaction.user.id;
-  const projectName = interaction.options.getString("name")!;
 
-  const { userId, error } = await getUserIdByDiscordUserId(discordUserId);
+  const { binding, error } = await getBindingByDiscordUserId(discordUserId);
   if (error) {
     await interaction.reply(errorEmbedReply(error));
     return;
   }
-
-  const res = await createProject(userId, projectName);
+  const userId = binding.owner_id as UserID;
+  if (!binding.active_project) {
+    await interaction.reply(errorEmbedReply("No active project found, use /activate-project to activate a project"));
+    return;
+  }
+  const res = await deleteProjectById(userId, binding.active_project);
   if (res?.error) {
     await interaction.reply(errorEmbedReply(res.error));
     return;
   }
 
-  await interaction.reply(infoEmbedReply("Project added successfully"));
+  await interaction.reply(
+    infoEmbedReply("Project deleted successfully", "Your active project has been deleted, select a new project with /activate-project")
+  );
 };
