eedf5812cb505c1ba5ae563b011d62220211ffc7
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-16 12:02:57 +0800

    feat: Add command to create a new column in the active project

---
+++ bot/src/commands/board/add-column.ts
@@ -0,0 +1,60 @@
+import {
+  type CacheType,
+  type ChatInputCommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
+import { errorEmbedReply, successEmbedReply } from "../../messages";
+import { addColumn } from "../../repo/columns";
+import { getBindingByDiscordUserId, type UserID } from "../../repo/users";
+import { columnNameValidate } from "../../utils/validators";
+
+export const data = new SlashCommandBuilder()
+  .setName("add-column")
+  .setDescription("Add a column to the active project")
+  .addStringOption((option) =>
+    option
+      .setName("column-name")
+      .setDescription("Column name")
+      .setRequired(true)
+  );
+
+export const execute = async (
+  interaction: ChatInputCommandInteraction<CacheType>
+) => {
+  console.log("add-column command executed");
+  const { binding, error: bindErr } = await getBindingByDiscordUserId(
+    interaction.user.id
+  );
+  if (bindErr) {
+    return await interaction.reply(
+      errorEmbedReply("An error occured while adding the column")
+    );
+  }
+  if (!binding.active_project) {
+    return await interaction.reply(
+      errorEmbedReply(
+        "No active project found, use /activate-project to activate a project"
+      )
+    );
+  }
+  const { data, error } = columnNameValidate.safeParse(
+    interaction.options.getString("column-name")
+  );
+  if (error) {
+    return await interaction.reply(errorEmbedReply(error.errors[0].message));
+  }
+
+  const res = await addColumn({
+    colName: data,
+    projectId: binding.active_project,
+    ownerId: binding.owner_id as UserID,
+  });
+
+  if (res) {
+    return await interaction.reply(errorEmbedReply(res.error));
+  }
+
+  return await interaction.reply(
+    successEmbedReply(`Column ${data} added successfully to the project`)
+  );
+};
