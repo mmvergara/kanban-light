7b7054ea719fd4cc542750a6a306e09f93577e9f
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-16 14:17:47 +0800

    refactor: Improve code formatting in ping command for better readability

--- bot/src/commands/helpers/ping.ts
+++ bot/src/commands/helpers/ping.ts
@@ -1,8 +1,17 @@
-import { type CacheType, type ChatInputCommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
+import {
+  type CacheType,
+  type ChatInputCommandInteraction,
+  MessageFlags,
+  SlashCommandBuilder,
+} from "discord.js";
 import { successEmbedReply } from "../../messages";
 
-export const data = new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!");
+export const data = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Replies with Pong!");
 
-export const execute = async (interaction: ChatInputCommandInteraction<CacheType>) => {
+export const execute = async (
+  interaction: ChatInputCommandInteraction<CacheType>
+) => {
   await interaction.reply(successEmbedReply("Pong!"));
 };
