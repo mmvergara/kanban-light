e8c51a4dc4f5408f1dbd1263714b28d6aa741686
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-16 08:59:06 +0800

    feat: Update create-project command to use getBindingByDiscordUserId for user identification

--- bot/src/commands/projects/create-project.ts
+++ bot/src/commands/projects/create-project.ts
@@ -1,7 +1,7 @@
 import { type CacheType, type ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { errorEmbedReply, infoEmbedReply } from "../../messages";
 import { createProject } from "../../repo/projects";
-import { getUserIdByDiscordUserId } from "../../repo/users";
+import { getBindingByDiscordUserId, type UserID } from "../../repo/users";
 
 export const data = new SlashCommandBuilder()
   .setName("create-project")
@@ -12,11 +12,13 @@ export const execute = async (interaction: ChatInputCommandInteraction<CacheType
   const discordUserId = interaction.user.id;
   const projectName = interaction.options.getString("name")!;
 
-  const { userId, error } = await getUserIdByDiscordUserId(discordUserId);
+  const { binding, error } = await getBindingByDiscordUserId(discordUserId);
   if (error) {
     await interaction.reply(errorEmbedReply(error));
     return;
   }
+
+  const userId = binding.owner_id as UserID;
 
   const res = await createProject(userId, projectName);
   if (res?.error) {
