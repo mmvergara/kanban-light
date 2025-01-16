f4d0055aea8c5e881fba40a1cfc25962f2cb033e
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-16 08:58:40 +0800

    feat: Add activate-project command to set a default project for column operations

---
+++ bot/src/commands/projects/activate-project.ts
@@ -0,0 +1,30 @@
+import { type CacheType, type ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { errorEmbedReply, infoEmbedReply } from "../../messages";
+import { activateProject, createProject } from "../../repo/projects";
+import { getBindingByDiscordUserId, type UserID } from "../../repo/users";
+
+export const data = new SlashCommandBuilder()
+  .setName("activate-project")
+  .setDescription("Activate a project this will be the default project for all column operations")
+  .addStringOption(option => option.setName("name").setDescription("The name of the project").setRequired(true));
+
+export const execute = async (interaction: ChatInputCommandInteraction<CacheType>) => {
+  const discordUserId = interaction.user.id;
+  const projectName = interaction.options.getString("name")!;
+
+  const { binding, error } = await getBindingByDiscordUserId(discordUserId);
+  if (error) {
+    await interaction.reply(errorEmbedReply(error));
+    return;
+  }
+  const userId = binding.owner_id as UserID;
+  const res = await activateProject(userId, projectName);
+  if (res?.error) {
+    await interaction.reply(errorEmbedReply(res.error));
+    return;
+  }
+
+  await interaction.reply(
+    infoEmbedReply(`Project ${projectName} is now active`, "All column operations will be performed on this project")
+  );
+};
